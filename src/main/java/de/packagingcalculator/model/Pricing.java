6cac1f046b23db92e4c1ad8d01964f37f8fc2bcc
Andreas Fuchs <Andrej.Fuchs@gmail.com>
2017-09-15 15:56:01 +0200

    Updated Constructor for Pricing

--- src/main/java/de/packagingcalculator/model/Pricing.java
+++ src/main/java/de/packagingcalculator/model/Pricing.java
@@ -3,12 +3,17 @@ package de.packagingcalculator.model;
 public class Pricing {
     private double[] dimensions;
     private int weight; //in gramm
+    private double price; //in €
 
-    public Pricing(double d1, double d2, double d3, int w){
+    public Pricing(double d1, double d2, double d3, int w, int p){
+        
+
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
     boolean fitsPackage(double d1, double d2, double d3, int w){
+
+
         throw new UnsupportedOperationException("Not yet implemented");
     }
 }
