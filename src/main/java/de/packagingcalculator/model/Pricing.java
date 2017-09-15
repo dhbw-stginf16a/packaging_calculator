3e5b6665999ffcfd4bd1f76aa5a2841784ab62d7
Andreas Fuchs <Andrej.Fuchs@gmail.com>
2017-09-15 16:07:20 +0200

    Added Initialization to Pricing Constructor, getPrice Method & created a routing to check if the dimensions fit the pricing category

--- src/main/java/de/packagingcalculator/model/Pricing.java
+++ src/main/java/de/packagingcalculator/model/Pricing.java
@@ -1,19 +1,42 @@
 package de.packagingcalculator.model;
 
+import java.util.Arrays;
+
 public class Pricing {
     private double[] dimensions;
     private int weight; //in gramm
     private double price; //in €
 
     public Pricing(double d1, double d2, double d3, int w, int p){
-        
+        dimensions[0] = d1;
+        dimensions[1] = d2;
+        dimensions[2] = d3;
+        Arrays.sort(dimensions);
 
-        throw new UnsupportedOperationException("Not yet implemented");
+        weight = w;
+        price = p;
     }
 
     boolean fitsPackage(double d1, double d2, double d3, int w){
+        boolean fits = false;
 
+        double[] packageDim = new double[3];
+        packageDim[0] = d1;
+        packageDim[1] = d2;
+        packageDim[2] = d3;
+        Arrays.sort(packageDim);
 
-        throw new UnsupportedOperationException("Not yet implemented");
+        if(packageDim[0] <= dimensions[0] &&
+                packageDim[1] <= dimensions[1] &&
+                packageDim[2] <= dimensions[2] &&
+                w <= weight) {
+            fits = true;
+        }
+
+        return fits;
+    }
+
+    public double getPrice() {
+        return price;
     }
 }
